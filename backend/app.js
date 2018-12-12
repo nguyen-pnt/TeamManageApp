1d6d39f836cd8c80683dfcc062bd901577202b92
nguyen-pnt <pntrnguyen@gmail.com>
2018-12-12 17:49:53 +0700

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
